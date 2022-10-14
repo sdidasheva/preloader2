61c797988e77861d47d0423dc20b9d66571d4cde
sdidasheva <ddsamals97@gmail.com>
2022-10-14 12:46:38 +0600

    commit

--- app/components/Preloader/Preloader.tsx
+++ app/components/Preloader/Preloader.tsx
@@ -5,7 +5,28 @@ import Background from "../Background/Background";
 const Preloader = () => {
 	return (
 		<Background>
-			<div></div>
+			<div className={styles.pageLoader}>
+				<div className={styles.pageLoader__content}>
+					<div className={styles.pageLoader__content_first}>
+						<h2>Мы кормим мир</h2>
+						<p>
+							Помогаем внедрять и применять фермерам{" "}
+							<strong>инновационные решения</strong>
+						</p>
+					</div>
+					<div className={styles.pageLoader__content_second}>
+						<h2>Развиваем сельское хозяйство</h2>
+						<p>
+							Для производство продукции <strong>наивысшее качество</strong>
+						</p>
+					</div>
+				</div>
+
+				<div className={styles.pageLoader__front}>
+					<div className={styles.pageLoader__back}></div>
+					<div className={styles.pageLoader__info}></div>
+				</div>
+			</div>
 		</Background>
 	);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import react from "react";
 import Preloader from "../app/components/Preloader/Preloader";
-
 export default function Home() {
 	return (
 		<>
